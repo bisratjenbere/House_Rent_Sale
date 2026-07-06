7e250876294903b01749e8925b95f670741b6204
= <jenberehenok@gmail.com>
2026-07-06 15:03:19 +0300

    Add barrel export for models and TypeScript interfaces

---
+++ models/index.ts
@@ -0,0 +1,23 @@
+// Barrel export file for all models and their TypeScript interfaces
+
+// Models
+export { default as User } from './User';
+export { default as Property } from './Property';
+export { default as Category } from './Category';
+export { default as Favorite } from './Favorite';
+export { default as Message } from './Message';
+export { default as Review } from './Review';
+export { default as Notification } from './Notification';
+export { default as Amenity } from './Amenity';
+export { default as ContactInquiry } from './ContactInquiry';
+
+// TypeScript interfaces
+export type { IUser } from './User';
+export type { IProperty } from './Property';
+export type { ICategory } from './Category';
+export type { IFavorite } from './Favorite';
+export type { IMessage } from './Message';
+export type { IReview } from './Review';
+export type { INotification } from './Notification';
+export type { IAmenity } from './Amenity';
+export type { IContactInquiry } from './ContactInquiry';
